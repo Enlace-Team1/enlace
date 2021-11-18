ec8d843dc8b16df3703c6775e40041cc239d743d
Jessica White <jessica2714white@gmail.com>
2021-11-18 16:41:35 -0600

    Add styling fo text wrap, top line, and heading

--- client/src/components/Bio/BioElements.js
+++ client/src/components/Bio/BioElements.js
@@ -47,3 +47,31 @@ export const Column2 = styled.div`
   grid-area: col2;
 `;
 
+export const TextWrapper = styled.div`
+  max-width: 540px;
+  padding-top: 0;
+  padding-bottom: 60px;
+`;
+
+export const TopLine = styled.div`
+  color: #01bf71;
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 700;
+  letter-spacing: 1.4px;
+  text-transform: uppercase;
+  margin-bottom: 16px;
+`;
+
+export const Heading = styled.h1`
+  margin-bottom: 24px;
+  font-size: 48px;
+  line-height: 1.1;
+  font-weight: 600;
+  color: '#f7f8fa';
+  /* color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#010606')}; */
+
+  @media screen and (max-width: 480px) {
+    font-size: 32px;
+  }
+`;
