9d44fd1a0ee5711ddfe1082140018ac9a85a0588
Jessica White <jessica2714white@gmail.com>
2021-11-17 21:50:14 -0600

    Change images

--- client/src/components/InfoSection/Data.js
+++ client/src/components/InfoSection/Data.js
@@ -3,14 +3,14 @@ export const homeObjOne = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'Enlace...',
+    topLine: 'Bonding ...',
     headline: '0% stress. 100% Fun',
     description:
       'Get access to our top-tier website that allows you to schedule events with your friends and family with no hassle.',
     buttonLabel: 'Get Started',
     imgStart: false,
-    img: require('../images/link.jpg').default,
-    alt: 'Link',
+    img: require('../images/calendar.png').default,
+    alt: 'Calendar',
     dark: true,
     primary: true,
     darkText: false
@@ -18,37 +18,37 @@ export const homeObjOne = {
   
   export const homeObjTwo = {
     id: 'about us',
-    lightBg: true,
-    lightText: false,
-    lightTextDesc: false,
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
     topLine: 'Behind The Screen',
     headline: 'Meet the Developers who Created this Application',
     description:
       'This dynamic team of ambitious coders have designed and built this application from scratch.',
-    buttonLabel: 'Learn More',
-    imgStart: true,
-    img: require('../images/mother.jpg').default,
+    buttonLabel: 'Connect Now',
+    imgStart: false,
+    img: require('../images/coders.jpg').default,
     alt: 'MotherBoard',
-    dark: false,
-    primary: false,
-    darkText: true
+    dark: true,
+    primary: true,
+    darkText: false
   };
   
   export const homeObjThree = {
     id: 'signup',
-    lightBg: true,
-    lightText: false,
-    lightTextDesc: false,
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
     topLine: 'Join our Social Network Today',
-    headline: 'Creating a profile is extremely easy',
+    headline: 'Creating a profile is a breeze',
     description:
       "Get everything set up and ready in under 10 minutes. All you need to do is add your information, invite your friends, and you're ready to go.",
     buttonLabel: 'Start Now',
     imgStart: false,
-    img: require('../images/matrix.jpg').default,
+    img: require('../images/login.png').default,
     alt: 'Sign Up',
-    dark: false,
-    primary: false,
-    darkText: true
+    dark: true,
+    primary: true,
+    darkText: false
   };
   
